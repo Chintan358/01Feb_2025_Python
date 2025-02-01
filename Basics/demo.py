0b84882320d38e429db4a2e19508c28f08ea2755
chintan <chintan.tops@gmail.com>
2025-02-01 08:12:38 +0530

    basics

---
+++ Basics/demo.py
@@ -0,0 +1,20 @@
+# print("Hello ' Python")
+# print('Helllo " Tops')
+# print("""Hello 
+#          world
+#       dsfdfdfd
+#       """)
+
+# print("python is \" oops lang.")
+# print('abc \' xytz')
+# print("abc \\ xyz")
+# print("abc \n zxyz")
+# print("abc\tzxyz")
+# print("abc\bdsds")
+
+
+
+# print("hello",end=" ")
+# print("python")
+
+# print("python","java","php","node",sep=" | ")
