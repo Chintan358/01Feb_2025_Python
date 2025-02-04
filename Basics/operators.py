8a3fae3ff12c754472248d94667ad4a8f2a337e7
chintan <chintan.tops@gmail.com>
2025-02-04 08:24:26 +0530

    operatos

---
+++ Basics/operators.py
@@ -0,0 +1,25 @@
+#Arithmatic operatos : 
+# +,-,*,/,%,**,//
+
+
+# print(10+10)
+# print("a"+"b")
+# print("a"+10)
+
+# a = 10+10.00
+# print(type(a))
+# print(a)
+
+# print(10%3)
+
+# print(10**2)
+# print(16//3)
+
+#Assignment Operatos
+
+# a = 10
+
+# a = a+20
+# a += 20
+# print(a)
+
