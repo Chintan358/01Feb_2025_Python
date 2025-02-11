ad42d6dbf89ab83e518e232b45ff3d3393a6acd4
chintan <chintan.tops@gmail.com>
2025-02-11 08:23:29 +0530

    logical

---
+++ 003_Logical/P1.py
@@ -0,0 +1,131 @@
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# lines = 7
+# for j in range(lines):
+#     for i in range(lines):
+#         print("*",end="")
+#     print()
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# lines = 5
+# for i in range(lines):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+# lines = 5
+# stars = 1
+# for i in range(lines):
+#     for j in range(stars):
+#         print("*",end="")
+#     print()
+#     stars+=1
+
+
+# *****
+# ****
+# ***
+# **
+# *
+
+# lines = 5
+# stars = lines
+# for i in range(lines):
+#     for j in range(stars):
+#         print("*",end="")
+#     print()
+#     stars-=1
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# lines = 5
+# stars = 1
+# space=lines-1
+# for i in range(lines):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         print("*",end="")
+#     print()
+#     stars+=1
+#     space-=1
+
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+
+# lines = 5
+# stars = lines
+# space=0
+# for i in range(lines):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         print("*",end="")
+#     print()
+#     stars-=1
+#     space+=1
+
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+# lines = 5
+# stars = 1
+# space=lines-1
+# for i in range(lines):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         print("*",end=" ")
+#     print()
+#     stars+=1
+#     space-=1
+
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+# lines = 5
+# stars = 1
+# space=lines-1
+# for i in range(lines):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         print("*",end="")
+#     print()
+#     stars+=2
+#     space-=1
+
+#   *
+#  ***
+# *****
+#  ***
+#   *
+
+#     *
+#    * *
+#   *   *
+#    * *
+#     *
