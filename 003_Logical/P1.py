47e95ac58e29a61a7ca473033a4b1b86d2cb4e3a
chintan <chintan.tops@gmail.com>
2025-02-13 08:36:35 +0530

    string

--- 003_Logical/P1.py
+++ 003_Logical/P1.py
@@ -129,3 +129,101 @@
 #   *   *
 #    * *
 #     *
+
+
+# lines = 7
+# stars = 1
+# space=lines-1
+# mid =( lines//2)+1
+# for i in range(1,lines+1):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         print("*",end="")
+#     print()
+#     if i<mid:
+#         stars+=2
+#         space-=1
+#     else:
+#         stars-=2
+#         space+=1
+
+
+# lines = 7
+# stars = 1
+# space=lines-1
+# mid =( lines//2)+1
+# for i in range(1,lines+1):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         if j==0 or j==stars-1:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+#     if i<mid:
+#         stars+=2
+#         space-=1
+#     else:
+#         stars-=2
+#         space+=1
+
+
+# *
+# **
+# * *
+# *  *
+# * *
+# **
+# *
+
+# lines = 7
+# stars = 1
+# space=lines-1
+# mid =( lines//2)+1
+# for i in range(1,lines+1):
+    
+#     for j in range(stars):
+#         if j==0 or j==stars-1:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+#     if i<mid:
+#         stars+=2
+#         space-=1
+#     else:
+#         stars-=2
+#         space+=1
+
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# 1
+# 2  3
+# 4  5  6
+# 7  8  9  10
+# 11 12 13 14 15
+
+# 0
+# 10
+# 010
+# 1010
+# 01010
+
+# 0
+# 01
+# 010
+# 0101
+# 01010
+
+# A 
+# BC 
+# DEF 
+# GHIJ 
+# KLMNO
