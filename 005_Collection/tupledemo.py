4a3b8b88359395ed65632767dc036a5fbb3b3655
chintan <chintan@gmail.com>
2025-02-28 08:57:40 +0530

    function

--- 005_Collection/tupledemo.py
+++ 005_Collection/tupledemo.py
@@ -19,6 +19,20 @@ t = ("python","php","android","ios")
 
 fruits = ("apple", "banana", "cherry")
 
-(a,*b) = fruits
+# (a,*b) = fruits
 
-print(b)
+# print(b)
+
+# for i in fruits:
+#     print(i)
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+t1 = (10,20,30)
+t2 = (80,90,70)
+
+# t3 = t1+t2
+# print(t3)
+# t3  = t1*3
+# print(t3)
