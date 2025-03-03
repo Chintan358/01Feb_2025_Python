92887608a54d7f030dcabcca5ab68098689cca98
chintan <chintan@gmail.com>
2025-03-03 09:00:32 +0530

    all

---
+++ 006_Function/module.py
@@ -0,0 +1,14 @@
+# import calc as c
+
+# k = c.add(10,20,30)
+# print(k)
+
+# from calc import *
+# from mapreducefilter import *
+
+import calc as c
+import mapreducefilter as m
+
+
+print(c.add(10,20))
+print(m.add(10))
