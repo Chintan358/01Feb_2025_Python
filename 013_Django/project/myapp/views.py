a0c8ebfab6b7a53751f8ed5fa0b1506d7d274237
chintan <chintan@gmail.com>
2025-04-23 08:30:00 +0530

    all

--- 013_Django/project/myapp/views.py
+++ 013_Django/project/myapp/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello")
-    return render(request,"index.html")
+    return render(request,"index.html",{'title':"tops | Index"})
 
 def about(request):
-    return render(request,"about.html")
+    return render(request,"about.html",{'title':"tops | About"})
 
 def contact(request):
-    return render(request,"contact.html")
+    return render(request,"contact.html",{'title':"tops | Contact"})
