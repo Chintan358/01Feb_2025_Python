1344fd9dc9846cb61efdb09cf0d99af4fad898f8
chintan <chintan@gmail.com>
2025-04-30 08:57:41 +0530

    all

--- 014_DjangoCRUD/myapp/admin.py
+++ 014_DjangoCRUD/myapp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from myapp.models import *
 # Register your models here.
-admin.site.register(Student)
+
+class StudentDisplay(admin.ModelAdmin):
+    list_display = ("username","email","phone","age")
+
+
+admin.site.register(Student,StudentDisplay)
--- 014_DjangoCRUD/myapp/views.py
+++ 014_DjangoCRUD/myapp/views.py
@@ -1,20 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from myapp.models import *
+import os
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    allStudents  = Student.objects.all()
+    return render(request,"index.html",{"students":allStudents})
 
 def register(request):
-    print("oky")
+   
+   
     if request.method=='POST':
         data = request.POST
+        id = data.get("id")
         username = data.get("uname")
         email = data.get("email")
         phone = data.get("phone")
         age = data.get("age")
+        image = request.FILES['file']
 
-        st =  Student.objects.create(username=username,email=email,phone=phone,age=age)
-        if(st):
-            return render(request,'index.html',{"msg":"Registration successful"})
+        if(id):
+            student = Student.objects.get(pk=id)
+            student.username=username
+            student.email= email
+            student.phone=phone
+            student.age=age
+            os.remove(student.image.path)
+            student.image=request.FILES['file']
+            student.save()
+            return redirect("index")
+        else:
+            st =  Student.objects.create(username=username,email=email,phone=phone,age=age,image=image)
+            if(st):
+                #return render(request,'index.html',{"msg":"Registration successful"})
+                return redirect("index")
 
     return render(request,'index.html')
+
+def delete(request):
+    stid =  request.GET['stid']
+    student = Student.objects.get(pk=stid)
+    os.remove(student.image.path)
+    student.delete()
+    return redirect("index")
+
+def update(request):
+    stid =  request.GET['stid']
+    student = Student.objects.get(pk=stid)
+    allStudents  = Student.objects.all()
+    return render(request,"index.html",{"student":student,"students":allStudents})
